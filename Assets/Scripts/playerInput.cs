9b665e078c3912198e6f816b24adebc6a43c2b45
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2016-06-30 16:42:03 +0200

    Work happened

--- Assets/Scripts/playerInput.cs
+++ Assets/Scripts/playerInput.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class playerInput : MonoBehaviour {
 
+	public float maxVelocityX = 15f;
+	public float minVelocityX = 6f;
 	public float jumpVelocity = 3f;
 	public float dashSpeed = 1f;
 	public float dashInterval = 0.25f;
 	public float wallSlideSpeed = 0.1f;
+	public float wallSlideSpeedLoss = 1f;
+	public float chargeSpeedGain = 0.2f;
+	public float chargeFriction = 0.4f;
+	public float smashTime = 1f;
 
 	public Rigidbody2D body { get; private set; }
 	public Animator anim { get; private set; }
@@ -19,6 +25,8 @@ public class playerInput : MonoBehaviour {
 	public bool wallSliding = false;
 	public bool goingRight = true;
 	public float dashCooldown = 0f;
+	public float smashCooldown = 0f;
+	public bool charging = false;
 
 	void Start () {
 		body = GetComponent<Rigidbody2D> ();
@@ -28,34 +36,63 @@ public class playerInput : MonoBehaviour {
 	void Update () {
 		velocity.y = body.velocity.y;
 		if (wallSliding) {
-			velX = 0.1f;
 			velocity.y = -wallSlideSpeed;
 		}
 		// Update
 		lastJumped += Time.deltaTime;
-		dashCooldown -= Time.deltaTime;
+		dashCooldown  = Mathf.Clamp (dashCooldown  - Time.deltaTime, -1, dashInterval);
+		smashCooldown = Mathf.Clamp (smashCooldown - Time.deltaTime, -1, smashTime);
 		// Handle player input
-		// - Jump
-		if (Input.GetKeyDown (KeyCode.UpArrow) && canJump) {
-			lastJumped = 0f;
-			grounded = false;
-			canJump = false;
-			velocity.y = jumpVelocity;
-			anim.SetTrigger("jump");
-			if (wallSliding)
-				turnAround();
-		}
-		// - Dash
-		if (Input.GetKeyDown (goingRight ? KeyCode.RightArrow : KeyCode.LeftArrow) && dashCooldown < 0f) {
-			dashCooldown = dashInterval;
-			velX += dashSpeed;
-			anim.SetTrigger("dash");
-			canJump = true;
-			if (wallSliding)
-				turnAround();
+		if (smashCooldown < 0f) {
+			// - Jump
+			if (canJump && Input.GetKeyDown (KeyCode.UpArrow)) {
+				lastJumped = 0f;
+				grounded = false;
+				canJump = false;
+				velocity.y = jumpVelocity;
+				anim.SetTrigger("jump");
+				if (velX < minVelocityX) velX = minVelocityX;
+				if (wallSliding) {
+					turnAround();
+				}
+			}
+			// - Dash
+			if (dashCooldown < 0f && Input.GetKeyDown (goingRight ? KeyCode.RightArrow : KeyCode.LeftArrow)) {
+				dashCooldown = dashInterval;
+				velX += dashSpeed;
+				anim.SetTrigger("dash");
+				canJump = true;
+				if (velX < minVelocityX) velX = minVelocityX;
+				if (wallSliding) {
+					turnAround();
+				}
+			}
+			// - Charge
+			if (dashCooldown < 0f && Input.GetKey (goingRight ? KeyCode.LeftArrow : KeyCode.RightArrow)) {
+				charging = true;
+				anim.SetBool ("charge", charging);
+				wallSliding = false;
+				anim.SetBool ("wallSlide", wallSliding);
+				velX += Time.deltaTime * chargeSpeedGain;
+			} else if (charging) {
+				smashCooldown = smashTime;
+				charging = false;
+				anim.SetBool ("charge", charging);
+				if (velX < minVelocityX) velX = minVelocityX;
+			}
 		}
 		// Update velocity
+		if (wallSliding) {
+			velX = Mathf.Clamp(velX - Time.deltaTime * wallSlideSpeedLoss, 0f, maxVelocityX);
+			dashCooldown = 0f;
+			smashCooldown = 0f;
+		}
+		if (velX > maxVelocityX)
+			velX = maxVelocityX;
 		velocity.x = goingRight ? velX : -velX;
+		if (charging) {
+			velocity.x *= chargeFriction;
+		}
 		body.velocity = velocity;
 		// Update animation variables
 		anim.SetFloat ("velX", Mathf.Abs(velocity.x));
@@ -70,24 +107,26 @@ public class playerInput : MonoBehaviour {
 	}
 
 	private void handleCollision(Collision2D coll) {
-		if (lastJumped > 0.01f) {
+		if (lastJumped > 0.05f && smashCooldown < 0f) {
 			if (!grounded || !wallSliding) {
-				ContactPoint2D contact = coll.contacts [0];
-				float contactFactor = Vector2.Dot(contact.normal, Vector2.up);
-				Debug.Log ("Dot: " + contactFactor);
-				if (contactFactor > 0.5f) {
-					grounded = true;
-					canJump = true;
-					anim.SetBool ("grounded", grounded);
-				} else if (contactFactor < 0.5f && contactFactor > -0.5f) {
-					if ((goingRight && contact.point.x > transform.position.x) || (!goingRight && contact.point.x < transform.position.x)) {
-						wallSliding = true;
+				foreach (ContactPoint2D contact in coll.contacts) {
+					float contactFactor = Vector2.Dot(contact.normal, Vector2.up);
+					Debug.Log ("Dot: " + contactFactor);
+					if (contactFactor > 0.5f && body.velocity.y <= 0f) {
+						grounded = true;
 						canJump = true;
-						anim.SetBool ("wallSlide", wallSliding);
+						anim.SetBool ("grounded", grounded);
+					} else if (contactFactor < 0.5f && contactFactor > -0.5f && !charging) {
+						if ((goingRight && contact.point.x > transform.position.x) || (!goingRight && contact.point.x < transform.position.x)) {
+							wallSliding = true;
+							canJump = true;
+							anim.SetBool ("wallSlide", wallSliding);
+						}
 					}
 				}
 			}
 			if (grounded && wallSliding) {
+				if (velX < minVelocityX) velX = minVelocityX;
 				turnAround();
 			}
 		}
@@ -96,7 +135,7 @@ public class playerInput : MonoBehaviour {
 	void OnCollisionEnter2D (Collision2D coll) {
 		handleCollision (coll);
 	}
-	void OnCollisionStay2D (Collision2D coll) {
+	void OnCollisionStay2D (Collision2D coll) { // FIXME NOT WORKING
 		handleCollision (coll);
 	}
 
